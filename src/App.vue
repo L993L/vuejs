374fc21e3aa27731c8de8f7ce3128a2a896a64c2
Lazar Markovic <lazar93kg@outlook.com>
2026-05-23 19:10:39 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>My Vue App</h1>
+        <button>Add Task</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+
+</script>
+
+<style>
+
+</style>
+
